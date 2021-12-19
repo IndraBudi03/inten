9b683b79c6ba8791efa259f2cedf108df8595001
unknown <indradino.mendunia@gmail.com>
2021-12-19 13:55:46 +0700

    Initial commit

---
+++ app/src/main/java/com/smkn9/intenp5/MainActivity.java
@@ -0,0 +1,15 @@
+package com.smkn9.intenp5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
